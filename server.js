79669f7a00195bc4dbd59aab6ec632d5b9344c67
jaylordlapena65-cell <jaylordlapena65@gmail.com>
2026-01-06 09:26:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,9 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
 const API = "http://bc1.ph1-mczie.fun:4094/server";
 const KEY = "GxzGQrGK1ZRKzhmGZRqoPOiqaEYuIrAIIxkkhXJAYXPSFmZZnd2RkwQm3nAIE7myDwwmeOQQ410osrKo2P1aNhf9BwTsac9IylN3";
 
-// Root route — para mawala ang "Cannot GET /"
-app.get("/", (req, res) => {
-  res.send("Barkada Backend is running 🚀");
-});
-
-// Status API
 app.get("/status", async (req, res) => {
   try {
     const r = await fetch(API, {
@@ -29,6 +21,8 @@ app.get("/status", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Backend running on port", PORT);
+  console.log("Backend running on", PORT);
 });
